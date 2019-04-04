45ed7d9cf25fd3ada6f8e55ad00558ad120e71ca
Ben Masters <mastersbd@gmail.com>
2019-04-03 18:48:10 -0700

    created ExtensionMethods and StorageSpace classes for read and storing ebooks

---
+++ eBookManager.Engine/ExtensionMethods.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookManager.Engine
+{
+    public static class ExtensionMethods
+    {
+        public static int ToInt(this string value, int defaultInteger = 0)
+        {
+            try
+            {
+                if (int.TryParse(value, out int validInteger))
+                {
+                    return validInteger;
+                }
+                else
+                {
+                    return defaultInteger;
+                }
+            }
+            catch
+            {
+                return defaultInteger;
+            }
+        }
+
+        public static double ToMegabytes(this long bytes)
+        {
+            return (bytes > 0) ? (bytes / 1024f) / 1024f : bytes; 
+        }
+
+        public static bool StorageSpaceExists(this List<StorageSpace> space, string nameValueToCheck, out int storageSpaceId)
+        {
+            bool exists = false;
+            storageSpaceId = 0;
+
+            if (space.Count() != 0)
+            {
+                int count = (from r in space where r.Name.Equals(nameValueToCheck) select r).Count();
+
+                if (count > 0)
+                {
+                    exists = true;
+                }
+                storageSpaceId = (from r in space select r.ID).Max() + 1;
+            }
+            return exists;
+        }
+
+        public static List<StorageSpace> ReadFromDataStore(this List<StorageSpace> value, string storagePath)
+        {
+            JsonSerializer json = new JsonSerializer();
+            if (!File.Exists(storagePath))
+            {
+                var newFile = File.Create(storagePath);
+                newFile.Close();
+            }
+            using (StreamReader sr = new StreamReader(storagePath))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    var retVal = json.Deserialize<List<StorageSpace>>(reader);
+                    if (retVal is null)
+                    {
+                        retVal = new List<StorageSpace>();
+
+                    }
+                    return retVal;
+                }
+            }
+        }
+    }
+}
